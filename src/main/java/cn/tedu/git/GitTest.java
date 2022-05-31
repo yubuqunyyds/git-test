1084ca5133eb8670db66bdaee5d713b93fe7aa7e
huangzy <huangzy@tedu.cn>
2022-05-31 20:40:22 +0800

    master commit2

--- src/main/java/cn/tedu/git/GitTest.java
+++ src/main/java/cn/tedu/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("Hello GitHub!");
         System.out.println("Hello EveryOne!");
         System.out.println("Hello Hot-fix1!");
+        System.out.println("Hello Hot-fix2！");
     }
 
 }
