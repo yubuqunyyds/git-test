84926054bf4d706ad4756e9ea20fc6f12fa6005c
huangzy <huangzy@tedu.cn>
2022-05-31 19:59:17 +0800

    third commit

--- src/main/java/cn/tedu/git/GitTest.java
+++ src/main/java/cn/tedu/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git！");
         System.out.println("Hello GitHub!");
+        System.out.println("Hello EveryOne!");
     }
 
 }
