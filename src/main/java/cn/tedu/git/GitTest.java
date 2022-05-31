eac5e58fc306361b60bd1ae2e0b41456562d2686
huangzy <huangzy@tedu.cn>
2022-05-31 19:58:16 +0800

    second commit

--- src/main/java/cn/tedu/git/GitTest.java
+++ src/main/java/cn/tedu/git/GitTest.java
@@ -3,6 +3,7 @@ package cn.tedu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git！");
+        System.out.println("Hello GitHub!");
     }
 
 }
